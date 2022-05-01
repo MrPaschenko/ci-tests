014b8d30ab3c136b19cbf3cfd7058ecc4fce3f92
mrpaschenko <paschenkod2@gmail.com>
2022-05-01 16:06:00 +0300

    Error handling

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -17,11 +17,18 @@ func main() {
 
 	// TODO: Change this to accept input from the command line arguments as described in the task and
 	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
+	handler := &lab2.ComputeHandler{
+		Input: {
+			//construct io.Reader according the command line parameters
+		},
+		Output: {
+			//construct io.Writer according the command line parameters
+		},
+	}
+	err := handler.Compute()
+	if err != nil {
+		println(err) // print err to stderr
+	}
 
 	res, _ := lab2.PrefixToPostfix("+ 2 2")
 	fmt.Println(res)
